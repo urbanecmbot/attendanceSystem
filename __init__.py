fd40cbe499c373a18f84e2e580210f895cacb8b8
Tomas Pelak <s3w3nofficial@gmail.com>
2017-04-21 00:37:15 +0200

    initial commit with some basic files

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("index.html", tittle='SimpleAttendanceSystem', body_tittle='Welcome to SimpleAttendanceSystem' )
+
+if __name__ == "__main__":
+    app.run()
