ff5f6161fab71cdb74bf6427426716f5a3c7932a
urbanecmbot <urbanecm3@gmail.com>
2017-04-22 21:56:05 +0200

    Remove useless commented debug line

--- __init__.py
+++ __init__.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    #dbStreamer.write('Lukas Valek', '31413414', '12:59:45');
-    #dbStreamer.read()
     card_UID = dbStreamer.getUID()
     return render_template("index.html", title='SimpleAttendanceSystem', cardUID=card_UID)
 
