d360e082397d5b777209254ba3ef860f6615f9b5
Martin Urbanec <martin@urbanec.cz>
2017-04-22 15:27:58 +0200

    Fix stucking error

--- logic/db.py
+++ logic/db.py
@@ -16,7 +16,7 @@ def write(name, card_id, time):
 	conn = connect()
 	cur = conn.cursor()
 	with cur:
-		cur.execute("insert into users (id, name, card_id, time) values (NULL, 'Lukas Valec', 1342210118, '12:59:59');", plain_query=True)
-		#cur.execute("insert into users (id, name, card_id, time) values (NULL, ?, ?, ?);", (name, card_id, time), plain_query=True)
+		#cur.execute("insert into users (id, name, card_id, time) values (NULL, 'Lukas Valec', 1342210118, '12:59:59');", plain_query=True)
+		cur.execute('''insert into users (id, name, card_id, time) values (NULL, "%s", "%s", "%s");''' % (name, card_id, time), plain_query=True)
 
 write('test', '31313', '12:59')
